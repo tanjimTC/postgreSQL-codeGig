37326c67be22f30802335383c2b08f1fba5efa02
tanjimTC <tanjimtc74@gmail.com>
2021-02-22 00:28:07 +0600

    handlebars views added

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
-const DB_PASS = process.env.DB_PASS;
-const DB_USERNAME = process.env.DB_USERNAME;
 const DB_NAME = process.env.DB_USERNAME;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASS = process.env.DB_PASS;
 
 module.exports = new Sequelize(DB_NAME, DB_USERNAME, DB_PASS, {
   host: "localhost",
