2b898c0c03b4174cbe6e72d2ca30d98ea970fd67
tanjimTC <tanjimtc74@gmail.com>
2021-02-22 00:26:20 +0600

    handlebars views added

--- config/database.js
+++ config/database.js
@@ -1,6 +1,10 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
+const DB_PASS = process.env.DB_PASS;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_NAME = process.env.DB_USERNAME;
 
-module.exports = new Sequelize("codegig", "postgres", "tanjim723526", {
+module.exports = new Sequelize(DB_NAME, DB_USERNAME, DB_PASS, {
   host: "localhost",
   dialect: "postgres",
 });
